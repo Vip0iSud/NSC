32d96c095d8cc61224d0ecb41cb931ff2c6299f4
maniacal <nutnotnit@gmail.com>
2020-11-02 19:09:17 +0700

    Main

---
+++ Assets/test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("i nut na hee hua kuy");
+    }
+
+    void Update()
+    {
+        
+    }
+}
